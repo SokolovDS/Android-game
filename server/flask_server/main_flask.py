b43545ee1db6d104280fbe79497d3880267d5314
artkholl <nes5813n58es13>
2017-05-28 13:29:51 +0500

    update server

--- server/flask_server/main_flask.py
+++ server/flask_server/main_flask.py
@@ -139,23 +139,94 @@ def spisok_list(local_spisok):
 
     if local_spisok != []:
 
-        for var in range(len(local_spisok)):
+        ret_var = '|'.join(local_spisok)
 
-            if var_2 == len(local_spisok) - 1 or var_2 == 0:
-                ret_var = ret_var + local_spisok[var_2]
-
-            elif var_2 != len(local_spisok) - 1:
-                ret_var = ret_var + local_spisok[var_2] + '|'
 
 
     return ret_var
 
+
+def role_gen(ses_id):   #1 - инквизитор; 0 - еретик | инквизиторв в сессии - 2; еретиков - 3
+
+    role_spisok = []
+    ret_var = ''
+
+    local_spisok = ses_dct[ses_id]
+
+    if local_spisok != []:
+
+        for var in local_spisok:
+
+            if role_dct != {}:
+                role_spisok.append(role_dct.get(var))
+
+            elif role_dct == {}:
+                ret_var = '1'
+                break
+
+        else:
+
+            #er = role_spisok.count('0')
+            ink = role_spisok.count('1')
+
+            if ink < 2:
+                ret_var = '1'
+
+            elif ink >= 2:
+                ret_var = '0'
+
+            else:
+                ret_var = 'error'
+
+    elif local_spisok == []:
+        ret_var = '1'
+
+    else:
+        ret_var = 'error'
+
+    return ret_var
+
+
+def ses_id_gen():
+
+    local_spisok = []
+    ret_var = ''
+
+    if ses_dct != {}:
+
+        for var in ses_dct:
+            local_spisok.append(var)
+
+        ses_id = randrange(100, 999)
+        ses_id = str(ses_id)
+
+        while ses_id in local_spisok:
+
+            ses_id = randrange(100, 999)
+            ses_id = str(ses_id)
+
+        ret_var = ses_id
+
+    elif ses_dct == {}:
+
+        ses_id = randrange(100, 999)
+        ses_id = str(ses_id)
+        ret_var = ses_id
+
+    else:
+        ret_var = 'Error'
+
+    return ret_var
 #------------------------------------------------------
 
 
-global coord_dct; coord_dct = {}
-global cur_users; cur_users = {}
-global cur_ses; cur_ses = {}
+global data_dct; data_dct = {}
+global ses_dct; ses_dct = {}
+global cur_users; cur_users = []
+global cur_ses; cur_ses = []
+
+global role_dct; role_dct = {} #1 - инквизитор; 0 - еретик
+global stat_dct; stat_dct = {}
 
 # -----------------------------------------------------
 
@@ -198,7 +269,7 @@ def send_email():
 
     if value.isdigit() == True:
         ret_var = value
-        cur_users[ret_var] = ''
+        cur_users.append(str(ret_var))
 
     else:
         ret_var = 'Error'
@@ -240,10 +311,9 @@ def get_cur_ses():
 
     if value == True:
 
-        if cur_ses != {}:
+        if cur_ses != []:
 
-            for var_1 in cur_ses:
-                local_spisok.append(str(var_1))
+            local_spisok = cur_ses.copy()
 
             ret_var = spisok_list(local_spisok)
 
@@ -256,31 +326,312 @@ def get_cur_ses():
     return json.dumps(ret_var)
 
 
-@app.route('/get_coord', methods = ['GET']) #Получение значений координат других участников, по своему id
+@app.route('/create_ses', methods = ['GET']) #Создание сессии
 
-def get_coord():
+def create_ses():
 
     get_id = request.args['idd']
-    get_coord = request.args['coord']
-
     get_id = str(get_id)
-    get_coord = str(get_coord)
+
 
     ret_var = ''
-    local_dct = {}
     local_spisok = []
 
     value = id_search(get_id)
 
     if value == True:
 
-        coord_dct[get_id] = get_coord
+        for var in ses_dct:
+            local_spisok.extend(ses_dct.get(var))
 
-        local_dct = coord_dct.copy()
+
+        if get_id not in local_spisok:
+
+
+            s_id = ses_id_gen()
+
+            cur_ses.append(s_id)
+
+            ses_dct[s_id] = [get_id]
+            ret_var = 'True'
+
+            var_role = role_gen(s_id)
+            role_dct[get_id] = var_role
+            stat_dct[get_id] = False
+            data_dct[get_id] = ''
+
+        else:
+            ret_var = 'False'
+
+    else:
+        ret_var = 'Error'
+
+    return json.dumps(ses_dct) #доделать
+
+
+@app.route('/join_ses', methods = ['GET']) #Подключение к сессии
+
+def join_ses():
+
+    get_id = request.args['idd']
+    ses_id = request.args['ses']
+
+    get_id = str(get_id)
+    ses_id = str(ses_id)
+
+    ret_var = ''
+    local_spisok = []
+    second_spisok = []
+    all_spisok = []
+
+
+    value = id_search(get_id)
+
+    if value == True:
+
+        local_spisok = ses_dct.get(ses_id)
+
+        if len(local_spisok) >= 5 or get_id in local_spisok:
+
+            ret_var = 'False'
+
+        elif len(local_spisok) < 5:
+
+            if ses_dct != {}:
+
+                for var in ses_dct:
+                   all_spisok.extend(ses_dct.get(var))
+
+
+            if get_id not in local_spisok and get_id not in all_spisok or all_spisok == []:
+
+                ret_var = 'True'
+                local_spisok.append(str(get_id))
+                ses_dct[str(ses_id)] = local_spisok
+
+                var_role = role_gen(ses_id)
+
+                role_dct[get_id] = var_role
+                stat_dct[get_id] = False
+                data_dct[get_id] = ''
+
+        else:
+            ret_var = 'Error'
+
+    else:
+        ret_var = 'Error'
+
+    return json.dumps(ses_dct) #Доделать
+
+
+@app.route('/role', methods = ['GET'])
+def role():
+
+    return json.dumps(str(role_dct) + str(stat_dct) + 'sessions' + str(ses_dct) + str(data_dct))
+
+
+@app.route('/get_user_in_ses', methods = ['GET'])
+
+def get_user_in_ses():
+
+    get_id = request.args['idd']
+    ses_id = request.args['ses']
+
+    ses_id = str(ses_id)
+    get_id = str(get_id)
+
+    local_spisok = []
+    user_spisok = []
+    ret_var = ''
+
+    value = id_search(get_id)
+
+    if value == True:
+
+        local_spisok = ses_dct.get(ses_id)
+
+        if get_id in local_spisok:
+            local_spisok.remove(get_id)
+
+        for var in local_spisok:
+            user_spisok.append(str(var) + '$' + str(role_dct.get(var)) + '$' + str(stat_dct.get(var)) )
+
+        ret_var = spisok_list(user_spisok)
+
+    else:
+        ret_var = 'Error'
+
+    return json.dumps(ret_var)
+
+
+@app.route('/get_stat_in_ses', methods = ['GET'])
+
+def get_stat_in_ses():
+
+    get_id = request.args['idd']
+    ses_id = request.args['ses']
+
+    ses_id = str(ses_id)
+    get_id = str(get_id)
+
+    local_spisok = []
+    stat_spisok = []
+    ret_var = ''
+
+    value = id_search(get_id)
+
+    if value == True:
+
+        local_spisok = ses_dct.get(ses_id)
+
+        if get_id in local_spisok:
+            local_spisok.remove(get_id)
+
+        for var in local_spisok:
+            stat_spisok.append(str(var) + '$' + str(stat_dct.get(var)))
+
+        ret_var = spisok_list(stat_spisok)
+
+    else:
+        ret_var = 'Error'
+
+    return json.dumps(ret_var)
+
+
+
+@app.route('/get_role_in_ses', methods = ['GET'])
+
+def get_role_in_ses():
+
+    get_id = request.args['idd']
+    ses_id = request.args['ses']
+
+    ses_id = str(ses_id)
+    get_id = str(get_id)
+
+    local_spisok = []
+    role_spisok = []
+    ret_var = ''
+
+    value = id_search(get_id)
+
+    if value == True:
+
+        local_spisok = ses_dct.get(ses_id)
+
+        if get_id in local_spisok:
+            local_spisok.remove(get_id)
+
+        for var in local_spisok:
+            role_spisok.append(str(var) + '$' + str(role_dct.get(var)))
+
+        ret_var = spisok_list(role_spisok)
+
+    else:
+        ret_var = 'Error'
+
+    return json.dumps(ret_var)
+
+
+
+@app.route('/leave_ses', methods = ['GET']) #Покидание сессии
+
+def leave_ses():
+
+    get_id = request.args['idd']
+    ses_id = request.args['ses']
+
+    local_spisok = []
+    ret_var = ''
+
+    value = id_search(get_id)
+
+    if value == True:
+
+        local_spisok = ses_dct.get(ses_id)
+
+        if get_id in local_spisok and len(local_spisok) == 1:
+
+            ses_dct.pop(ses_id, None)
+            role_dct.pop(get_id, None)
+            stat_dct.pop(get_id, None)
+
+            local_spisok.remove(get_id)
+
+            ret_var = True
+
+        else:
+            role_dct.pop(get_id, None)
+            stat_dct.pop(get_id, None)
+
+            local_spisok.remove(get_id)
+
+            ret_var = True
+
+    else:
+        ret_var = 'error'
+
+
+    return json.dumps(ses_dct)
+
+
+
+@app.route('/stat_true', methods = ['GET'])
+
+def stat_true():
+
+    get_id = request.args['idd']
+    get_id = str(get_id)
+
+    ret_var = ''
+
+    value = id_search(get_id)
+
+    if value == True:
+
+        stat_dct[get_id] = True
+        ret_var = 'True'
+
+    else:
+        ret_var = 'Error'
+
+    return json.dumps(ret_var)
+
+
+
+@app.route('/get_data', methods = ['GET']) #Получение значений координат других участников, по своему id
+
+def get_data():
+
+    get_id = request.args['idd']
+    get_ses = request.args['ses']
+    get_coord = request.args['coord']
+
+    get_id = str(get_id)
+    get_ses = str(get_ses)
+    get_coord = str(get_coord)
+
+    ret_var = ''
+    local_dct = {}
+    local_spisok = []
+    user_spisok = []
+
+    value = id_search(get_id)
+
+    if value == True:
+
+        data_dct[get_id] = get_coord
+
+        user_spisok = ses_dct.get(get_ses)
+
+        local_dct = data_dct.copy()
         local_dct.pop(get_id, None)
 
-        for var in local_dct:
-            local_spisok.append(local_dct.get(var))
+        if user_spisok != []:
+
+            for var in user_spisok:
+                if local_dct != {} and var != get_id:
+                    local_spisok.append(local_dct.get(var))
 
         ret_var = spisok_list(local_spisok)
 
@@ -302,17 +653,29 @@ def logout():
     get_id = request.args['idd']
     get_id = str(get_id)
 
+    value = id_search(get_id)
 
-    return json.dumps('True')
+    if value == True:
+        try:
+            cur_users.remove(get_id)
+        except:
+            None
+
+    else:
+        None
+
+    return json.dumps(cur_users)
 
 
 @app.route('/clear_coord', methods = ['GET'])
 
 def clear_coord():
 
-    coord_dct.clear()
+    data_dct.clear()
+    ses_dct.clear()
+    cur_se.clear()
 
-    return json.dumps(coord_dct)
+    return json.dumps(data_dct)
 
 if __name__ == '__main__':
     app.run()
