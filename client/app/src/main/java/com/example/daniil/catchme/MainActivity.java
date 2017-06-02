459311b954be20a1087a66d5f9c537fc50e7c807
Daniil <sokolov.exe@gmail.com>
2017-06-03 00:02:04 +0500

    Authorization OK

--- client/app/src/main/java/com/example/daniil/catchme/MainActivity.java
+++ client/app/src/main/java/com/example/daniil/catchme/MainActivity.java
@@ -8,22 +8,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity {
-    EditText eText;
-    TextView tvInfo;
+    EditText eText = (EditText) findViewById(R.id.editText);
+    TextView tvInfo= (TextView) findViewById(R.id.tvInfo);
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        eText = (EditText) findViewById(R.id.editText);
-        tvInfo = (TextView) findViewById(R.id.tvInfo);
 
         //Подключаем нажатие на кнопки
         //Кнопка никнейм
@@ -69,13 +75,39 @@ public class MainActivity extends AppCompatActivity {
     private final View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            //ТЕСТИМ VALLEY
+            RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+
+            String url = "http://artkholl.pythonanywhere.com/send_email?email="+eText.getText().toString();
+
+            StringRequest jsObjRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+
+                @Override
+                public void onResponse(String response) {
+                    // TODO Auto-generated method stub
+                    tvInfo.setText("Response => "+response);
+                }
+            }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    // TODO Auto-generated method stub
+                    tvInfo.setText("ERROR 1");
+
+                }
+            });
+            //---------------------------------------
             switch (v.getId()) {
                 case R.id.NickBTN:
-                    String str = eText.getText().toString();
+                    /*String str = eText.getText().toString();
                     Toast msg = Toast.makeText(getBaseContext(),str,Toast.LENGTH_LONG);
                     msg.show();
                     MyTask mt = new MyTask();
-                    mt.execute();
+                    mt.execute();*/
+
+                    queue.add(jsObjRequest);
+
+
                     break;
                 case R.id.FindSession:
                     Intent fSession = new Intent(MainActivity.this, findSession.class);
