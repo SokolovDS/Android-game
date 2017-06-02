3f9ab78764035fb5b666788b5c9e782c91e17fae
Daniil <sokolov.exe@gmail.com>
2017-06-02 12:53:21 +0500

    Ввод нинкейма

--- client/app/src/main/java/com/example/daniil/catchme/MainActivity.java
+++ client/app/src/main/java/com/example/daniil/catchme/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.daniil.catchme;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText eText;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Считывание из строки
+        eText = (EditText)findViewById(R.id.NickName);
+
+        btn = (Button) findViewById(R.id.txtBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String str = eText.getText().toString();
+                Toast msg = Toast.makeText(getBaseContext(),str,Toast.LENGTH_LONG);
+                msg.show();
+            }
+        });
+
 
         //Подключаем нажатие на кнопки
         Button findSessionBtn = (Button) findViewById(R.id.FindSession);
