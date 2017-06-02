50c2c95875987cdbf219ed44e60a861125bc05fe
Daniil <sokolov.exe@gmail.com>
2017-06-02 16:17:49 +0500

    async task test

--- client/app/src/main/java/com/example/daniil/catchme/MainActivity.java
+++ client/app/src/main/java/com/example/daniil/catchme/MainActivity.java
@@ -1,36 +1,35 @@
 package com.example.daniil.catchme;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
 
 public class MainActivity extends AppCompatActivity {
     EditText eText;
-    Button btn;
+    TextView tvInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Считывание из строки
-        eText = (EditText)findViewById(R.id.NickName);
-
-        btn = (Button) findViewById(R.id.txtBtn);
-        btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String str = eText.getText().toString();
-                Toast msg = Toast.makeText(getBaseContext(),str,Toast.LENGTH_LONG);
-                msg.show();
-            }
-        });
-
+        eText = (EditText) findViewById(R.id.editText);
+        tvInfo = (TextView) findViewById(R.id.tvInfo);
 
         //Подключаем нажатие на кнопки
+        //Кнопка никнейм
+        Button nicknameBtn = (Button) findViewById(R.id.NickBTN);
+        nicknameBtn.setOnClickListener(onClickListener);
+        //остальные кнопки
         Button findSessionBtn = (Button) findViewById(R.id.FindSession);
         findSessionBtn.setOnClickListener(onClickListener);
         Button createSessionBtn = (Button) findViewById(R.id.CreateSession);
@@ -38,12 +37,46 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    class MyTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            tvInfo.setText("Begin");
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            tvInfo.setText("End");
+        }
+    }
+
+
+
     //Трекер нажатий
     private final View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
-
+                case R.id.NickBTN:
+                    String str = eText.getText().toString();
+                    Toast msg = Toast.makeText(getBaseContext(),str,Toast.LENGTH_LONG);
+                    msg.show();
+                    MyTask mt = new MyTask();
+                    mt.execute();
+                    break;
                 case R.id.FindSession:
                     Intent fSession = new Intent(MainActivity.this, findSession.class);
                     startActivity(fSession);
--- client/app/src/main/java/com/example/daniil/catchme/maps.java
+++ client/app/src/main/java/com/example/daniil/catchme/maps.java
@@ -2,8 +2,9 @@ package com.example.daniil.catchme;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -46,20 +47,11 @@ public class maps extends FragmentActivity implements OnMapReadyCallback {
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.setMyLocationEnabled(true);
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (MY_LOCATION_REQUEST_CODE == requestCode) {
-            if (permissions.length == 1 &&
-                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
-                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                mMap.setMyLocationEnabled(true);
-            } else {
-                // Permission was denied. Display an error message.
-            }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            // Show rationale and request permission.
         }
     }
 }
