3ee78ce39e8e9c2cc66a814324cc11464dcf3851
Daniil <sokolov.exe@gmail.com>
2017-06-03 03:20:13 +0500

    OMG it works

--- client/app/src/main/java/com/example/daniil/catchme/MainActivity.java
+++ client/app/src/main/java/com/example/daniil/catchme/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.daniil.catchme;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -16,14 +15,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import java.util.concurrent.TimeUnit;
-
-
 public class MainActivity extends AppCompatActivity {
-    EditText eText = (EditText) findViewById(R.id.editText);
-    TextView tvInfo= (TextView) findViewById(R.id.tvInfo);
-
-
 
 
     @Override
@@ -34,53 +26,29 @@ public class MainActivity extends AppCompatActivity {
         //Подключаем нажатие на кнопки
         //Кнопка никнейм
         Button nicknameBtn = (Button) findViewById(R.id.NickBTN);
+
         nicknameBtn.setOnClickListener(onClickListener);
         //остальные кнопки
         Button findSessionBtn = (Button) findViewById(R.id.FindSession);
         findSessionBtn.setOnClickListener(onClickListener);
         Button createSessionBtn = (Button) findViewById(R.id.CreateSession);
         createSessionBtn.setOnClickListener(onClickListener);
-
     }
-
-
-    class MyTask extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            tvInfo.setText("Begin");
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            try {
-                TimeUnit.SECONDS.sleep(2);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-            tvInfo.setText("End");
-        }
-    }
-
-
 
     //Трекер нажатий
     private final View.OnClickListener onClickListener = new View.OnClickListener() {
+        EditText eText;
+        TextView tvInfo;
         @Override
         public void onClick(View v) {
-            //ТЕСТИМ VALLEY
+            eText = (EditText) findViewById(R.id.editText);
+            tvInfo = (TextView) findViewById(R.id.tvInfo);
+            //ТЕСТИМ VOLLEY
             RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
 
             String url = "http://artkholl.pythonanywhere.com/send_email?email="+eText.getText().toString();
 
-            StringRequest jsObjRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
 
                 @Override
                 public void onResponse(String response) {
@@ -99,24 +67,16 @@ public class MainActivity extends AppCompatActivity {
             //---------------------------------------
             switch (v.getId()) {
                 case R.id.NickBTN:
-                    /*String str = eText.getText().toString();
-                    Toast msg = Toast.makeText(getBaseContext(),str,Toast.LENGTH_LONG);
-                    msg.show();
-                    MyTask mt = new MyTask();
-                    mt.execute();*/
-
-                    queue.add(jsObjRequest);
-
-
+                    queue.add(stringRequest);
                     break;
                 case R.id.FindSession:
                     Intent fSession = new Intent(MainActivity.this, findSession.class);
                     startActivity(fSession);
                     break;
-                case R.id.CreateSession:
+                /*case R.id.CreateSession:
                     Intent cSession = new Intent(MainActivity.this, createSession.class);
                     startActivity(cSession);
-                    break;
+                    break;*/
 
 
                 default:
